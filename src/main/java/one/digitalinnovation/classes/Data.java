2391d12dc3237339d6854470eb3b41cc5da97df1
Yuri Magalhaes <89856842+yurimagalhaesrc@users.noreply.github.com>
2021-09-10 10:34:02 -0300

    First commit, project examples,working for date

---
+++ src/main/java/one/digitalinnovation/classes/Data.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation.classes;
+
+import java.util.Date;
+public class Data {
+    public static void main(String[] args){
+        Date novaData = new Date();
+        System.out.println(novaData);
+    }
+}
